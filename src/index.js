f68d731b404ffac7c5ad79dfe3af20ca0f7a248e
AmroNagdy <amro.nagdy@gmail.com>
2020-05-20 17:16:59 +0100

    Remove debug tool.

--- src/index.js
+++ src/index.js
@@ -2,17 +2,14 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
-import { createStore, applyMiddleware, compose } from 'redux';
+import { createStore, applyMiddleware } from 'redux';
 import allReducers from './reducers';
 import { Provider } from 'react-redux';
 import thunk from 'redux-thunk';
 
 const store = createStore(
   allReducers,
-  compose(
-    applyMiddleware(thunk),
-    window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
-  )
+  applyMiddleware(thunk)
 );
 
 const rootElement = document.getElementById('root');
