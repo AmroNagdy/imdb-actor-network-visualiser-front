e7d9e31e529ddff46101579aa8326197486d8a1b
AmroNagdy <amro.nagdy@gmail.com>
2020-05-20 12:03:07 +0100

    Update backend base uri again.

--- src/constants/RestApi.js
+++ src/constants/RestApi.js
@@ -1,3 +1,3 @@
-export const BASE_URI = 'https://imdb-actor-network-visualiser.eba-fmrb5b33.eu-west-2.elasticbeanstalk.com/api/';
+export const BASE_URI = 'imdb-actor-network-visualiser.eba-fmrb5b33.eu-west-2.elasticbeanstalk.com/api/';
 export const SEARCH_ACTORS_BY_NAME = 'search-actors-by-name';
 export const GET_NETWORK_BY_NCONST = 'get-network-by-nconst';
