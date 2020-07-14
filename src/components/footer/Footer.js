1653caccff9acfc7c99ee2ae7f22dd5f3baf0f6e
AmroNagdy <amro.nagdy@gmail.com>
2020-07-14 21:59:08 +0100

    Add instructions and background colour.

--- src/components/footer/Footer.js
+++ src/components/footer/Footer.js
@@ -1,16 +1,27 @@
 import React from 'react';
 import GitHubLogo from './GitHubLogo.svg';
 import GitHubLogoStyle from '../styles/GitHubLogoStyle';
+import StyledInstructions from '../styles/StyledInstructions';
 
 export default function Footer() {
 
+
+
   return (
-    <a href='https://github.com/AmroNagdy/imdb-actor-network-visualiser-front'>
-      <GitHubLogoStyle
-        src={GitHubLogo}
-        alt='GitHubLogo'
-      />
-    </a>
+    <>
+      <StyledInstructions>
+        <b>Instructions</b><br />
+        1. Lookup an actor by name.<br />
+        2. Select the actor that you want.<br />
+        3. Mouse-over or tap a node in the network to see what films a connected actor has featured in.<br />
+      </StyledInstructions>
+      <a href='https://github.com/AmroNagdy/imdb-actor-network-visualiser-front'>
+        <GitHubLogoStyle
+          src={GitHubLogo}
+          alt='GitHubLogo'
+        />
+      </a>
+    </>
   );
 
 };
---
+++ src/components/styles/StyledInstructions.js
@@ -0,0 +1,10 @@
+import styled from 'styled-components';
+import { LIGHT_GRAY } from '../../constants/AppColours';
+
+export default styled.p`
+  align-self: center;
+  text-align: center;
+  justify-self: center;
+  color: ${LIGHT_GRAY};
+  font-size: 1rem;
+`;
